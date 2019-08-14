cb55b282199214803a8d5ff50bb1c3bb025daecf
Ping-Ke Shih <pkshih@realtek.com>
2019-08-14 17:45:34 +0800

    rtw88: Don't need send additional information to legacy firmware
    The firmware of 11AC chip needs more information to support more offload
    functions, such as IQK. 8723D implement these functions in driver instead,
    so don't need to send these additional information to firmware.
    
    Change-Id: If6bd465c377632122054f5be00c04b75e8314b32
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>

--- fw.c
+++ fw.c
@@ -266,6 +266,9 @@ rtw_fw_send_general_info(struct rtw_dev *rtwdev)
 	u8 h2c_pkt[H2C_PKT_SIZE] = {0};
 	u16 total_size = H2C_PKT_HDR_SIZE + 4;
 
+	if (rtwdev->chip->wlan_cpu == RTW_WCPU_11N)
+		return;
+
 	rtw_h2c_pkt_set_header(h2c_pkt, H2C_PKT_GENERAL_INFO);
 
 	SET_PKT_H2C_TOTAL_LEN(h2c_pkt, total_size);
@@ -285,6 +288,9 @@ rtw_fw_send_phydm_info(struct rtw_dev *rtwdev)
 	u8 h2c_pkt[H2C_PKT_SIZE] = {0};
 	u16 total_size = H2C_PKT_HDR_SIZE + 8;
 	u8 fw_rf_type = 0;
+
+	if (rtwdev->chip->wlan_cpu == RTW_WCPU_11N)
+		return;
 
 	if (hal->rf_type == RF_1T1R)
 		fw_rf_type = FW_RF_1T1R;
